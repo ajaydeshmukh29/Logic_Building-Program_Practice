65be1b52b84988c26a9955991dcd311ced8df0c2
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-05-25 22:35:17 +0530

    for loop

---
+++ program61.c
@@ -0,0 +1,27 @@
+// Input : 5
+// Output : -5 -4 -3 -2 -1 
+
+#include<stdio.h>
+
+void Display(int iNO)
+{
+  int iCnt = 0;
+
+  for(iCnt = -iNO; iCnt < 0; iCnt++)
+  {
+    printf("%d\t",iCnt);
+  }
+  printf("\n");
+}
+
+int main()
+{
+
+  int iValue = 0;
+
+  printf("Enter number : \n");
+  scanf("%d",&iValue);
+
+  Display(iValue);
+  return 0;
+}
