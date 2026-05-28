84d88cf76edfc09c02a9d18e0f7002534b23ec03
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-05-28 22:49:53 +0530

    while loop

---
+++ program74.c
@@ -0,0 +1,16 @@
+// Input : 5
+// Output :  5 4 3 2 1
+#include<stdio.h>
+
+int main()
+{
+  int iCnt = 5;
+
+  while(iCnt >= 1)
+  {
+    printf("%d\t",iCnt);
+    iCnt--;
+  }
+  printf("\n");
+  return 0;
+}
