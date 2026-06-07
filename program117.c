ceed6157bb241048a2d36e720eae2010c1a9f543
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-06-07 18:07:29 +0530

    Array

---
+++ program117.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void Display(int Arr[], int iSize)  
+{
+  int iCnt = 0;
+
+  for(iCnt = 0; iCnt < iSize; iCnt++)
+  {
+    printf("%d\n",Arr[iCnt]);
+  }
+}
+
+int main()
+{
+  int iLenght = 4;
+
+  int Brr[iLenght];
+
+  int iCnt = 0;
+
+  printf("Enter the Elements : \n");
+
+  for(iCnt = 0; iCnt < iLenght; iCnt++)
+  {
+    scanf("%d",&Brr[iCnt]);
+  }
+
+  Display(Brr,iLenght); 
+
+  return 0;
+}
