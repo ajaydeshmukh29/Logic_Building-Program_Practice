ee8f1a5cc3fb38c672e59c5bd518685d9171b1db
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-06-07 18:09:44 +0530

    Memory allocation steps

---
+++ program119.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+// Error due to header file
+
+int main()
+{
+  int *Brr = NULL;
+  int iLength = 0, iCnt = 0;
+
+  // Step 1 : Accept the number elements
+  printf("Number of elements : \n");
+  scanf("%d",&iLength);
+
+  // Step 2 : Allocate the memory 
+  Brr = (int *)malloc(iLength * sizeof(int));
+
+  // Step 3 : Accept the value form user
+  for(iCnt = 0; iCnt < iLength; iCnt++)
+  {
+    scanf("%d",&Brr[iCnt]);
+  }
+
+  // Step 4 : Use the memory (LOGIC)
+
+  // Step 5 : Deallocate the memory
+  free(Brr);
+
+  return 0;
+}
