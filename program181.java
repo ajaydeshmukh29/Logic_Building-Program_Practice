3163b5a4c0eba46970e6bb36ac58ceec779aba98
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-06-16 14:08:42 +0530

    Printing Charactor

---
+++ program181.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class program181
+{
+  public static void Display(int iNO)
+  {
+    int iCnt = 0;
+    char ch = '\0';
+
+    for(iCnt = 1, ch = 97; iCnt <= iNO; iCnt++, ch++)
+    { 
+      System.out.print(ch+"\t");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) 
+  {
+       Scanner sobj = new Scanner(System.in);
+       int iValue = 0;
+
+       System.out.println("ente the number : ");
+       iValue = sobj.nextInt();
+
+       Display(iValue);
+  }
+  
+} 
