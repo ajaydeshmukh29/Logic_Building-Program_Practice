05d4cf018f4c7a52786d858aa8365f9fab457f81
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-06-16 14:08:02 +0530

    Printing small letter

---
+++ program180.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class program180
+{
+  public static void Display(int iNO)
+  {
+    int iCnt = 0;
+    char ch = '\0';
+
+    for(iCnt = 1, ch = 'a'; iCnt <= iNO; iCnt++, ch++)
+    { 
+      System.out.print(ch+"\t");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) 
+  {
+       Scanner sobj = new Scanner(System.in);
+       int iValue = 0;
+
+       System.out.println("ente the number : ");
+       iValue = sobj.nextInt();
+
+       Display(iValue);
+  }
+  
+} 
