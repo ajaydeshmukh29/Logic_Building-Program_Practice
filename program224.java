6b443051ce3a5fcb59b3172e03ba3a52be9bb14a
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-07-04 23:40:27 +0530

    Print lower triangle number with rowise

---
+++ program224.java
@@ -0,0 +1,51 @@
+/*
+  iRow = 4
+  icol = 4
+
+1
+1 2 
+1 2 3
+1 2 3 4
+
+*/
+
+import java.util.*;
+
+class Pattern
+{
+  public void Display(int iRow, int iCol)
+  {
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+      for(j = 1; j <= i; j++)
+      {
+          System.out.print(j+"\t");
+      }
+      System.out.println();
+      
+    }
+  }
+}
+
+public class program224
+{
+  public static void main(String A[])
+  {
+    Scanner sobj = new Scanner(System.in);
+    int iValue1 = 0, iValue2 = 0;
+    
+    System.out.println("Enter number of Row : ");
+    iValue1 = sobj.nextInt();
+
+    System.out.println("Enter number of column : ");
+    iValue2 = sobj.nextInt();
+
+    Pattern pobj = new Pattern();
+
+    pobj.Display(iValue1, iValue2);
+
+  }
+  
+}
