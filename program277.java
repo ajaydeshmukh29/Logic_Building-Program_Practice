b0516b85961f9936c177b149d2c0babcd52f64b4
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-07-05 23:09:59 +0530

    Charactor printing

---
+++ program277.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import Marvellous.StringX;
+
+class program277
+{
+  public static void main(String A[])
+    {
+      Scanner sobj = new Scanner(System.in);
+      String data = null;
+      program280 strobj = new program280();   // Error
+      int iRet = 0;
+
+      System.out.println("Enter string : ");
+      data = sobj.nextLine();
+
+      iRet = strobj.CountCapital(data);
+
+      System.out.println("Number of capital characters : "+iRet);
+
+       iRet = strobj.CountSmall(data);
+
+       System.out.println("Number of small characters : "+iRet);
+
+       iRet = strobj.CountDigits(data);
+
+       System.out.println("Number of Digits are : "+iRet);
+
+       iRet = strobj.CountSpace(data);
+
+       System.out.println("Number of White spaces  are : "+iRet);
+      iRet = strobj.CountSpecial(data);
+
+       System.out.println("Number of special symbol are : "+iRet);
+
+    }
+
+}
