9be8d22009e20dc45d1bb0157ab08f1e922ba497
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-07-07 23:30:58 +0530

    Length of string

---
+++ program234.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main()
+{
+  char str[] = "Jay Ganesh";
+  int iRet = 0;
+
+  iRet = strlen(str);
+
+  printf("Lenght of string is : %d\n",strlen(str));
+
+  return 0;
+}
