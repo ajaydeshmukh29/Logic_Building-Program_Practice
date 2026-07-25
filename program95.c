c2127df62138bc2548e2df6dbf51ca6f50099907
Ajay Deshmukh <deshmukhajay950@gmail.com>
2026-07-25 21:21:50 +0530

    Fix sizeof calculation output for Arr array

--- program95.c
+++ program95.c
@@ -1,10 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application which calculate the sizeof Arr[] array.
+//
+//  Reading:
+//  Arr is a one-dimentional array which contains 7 elements.Each element is of type integer.
+//
+//  Calculation: 
+//  sizeof(Arr) = sizeof(data type int) * Number of elements(7)
+//              = 28 bytes
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
 int main()
 {
   int Arr[7] = {10,20,30,40,50};
 
-  printf("%d\n",sizeof(Arr)); //20
+  printf("%d\n",sizeof(Arr)); //28
 
   return 0;
 }
