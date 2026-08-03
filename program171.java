25e0dda7c064cde0430cf88fac033ed742b34e6c
Ajay Deshmukh <deshmukhajay950@gmail.com>
2026-08-03 23:46:30 +0530

    Fix formatting in Display method

--- program171.java
+++ program171.java
@@ -5,7 +5,7 @@ class program171
     public static void Display(int iNo)
     {
         int iCnt = 0;
-
+        
         for(iCnt = 1; iCnt <= iNo; iCnt++)
         {
             System.out.print("*\t");
